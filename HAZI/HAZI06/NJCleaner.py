67b33cb203a292dd821bfc34717c097435365021
Benedek <b.benedek2000@gmail.com>
2023-04-14 15:33:05 +0200

    HAZI 06, 2. feladat osszes

--- HAZI/HAZI06/NJCleaner.py
+++ HAZI/HAZI06/NJCleaner.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math as ma
 
 class NJCleaner():
     
@@ -15,8 +16,9 @@ class NJCleaner():
         return cleanedDf
     
     def convert_date_to_day(self) -> pd.DataFrame:
-        self.data['day'] = pd.to_datetime(self.data['day']).dt.day_name()
-        convertedDf = self.data.drop('date', axis=1)
+        convertedDf = self.data
+        convertedDf['day'] = pd.to_datetime(convertedDf['day']).dt.day_name()
+        convertedDf = convertedDf.drop('date', axis=1)
         return convertedDf
 
     def convert_scheduled_time_to_part_of_the_day(self) -> pd.DataFrame:
@@ -28,7 +30,35 @@ class NJCleaner():
         convertDf = convertDf.drop(["scheduled_time"], axis=1)
         return convertDf
     
+
+    def convert_delay(self) -> pd.DataFrame:
+        delayDf = self.data
+        delayDf['delay'] = pd.cut(delayDf['delay_minutes'],
+                                bins=[-1, 5, ma.inf],
+                                labels=[0, 1])
+        return delayDf
     
+    def drop_unnecessary_columns(self) -> pd.DataFrame:
+        dropColDf = self.data.drop(['train_id', 'actual_time', 'delay_minutes'],
+                                   axis=1)
+        return dropColDf
+    
+    def save_first_60k(self, save_path: str):
+        save_df = self.data.head(60000)
+        return save_df.to_csv(path_or_buf=save_path, index=False)
+    
+    def prep_df(self, save_path: str = 'data/NJ.csv'):
+        self.data = (self.order_by_scheduled_time(),
+                    self.drop_columns_and_nan(),
+                    self.convert_date_to_day(),
+                    self.convert_scheduled_time_to_part_of_the_day(),
+                    self.convert_delay(),
+                    self.drop_unnecessary_columns(),
+                    self.save_first_60k(save_path))
+
+    
+
+
 
 
 
